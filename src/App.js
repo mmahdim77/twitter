f2abfb877e0176b04792c4296c43d689a9a10000
atousa havaseli <atoosa.havaseli@gmail.com>
2021-01-24 12:13:43 +0330

    app chnages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import PostCard from './components/post-card.components/post-card.components'
 import CustomButton from './components/button.components/button.components'
 function App() {
   const list = ['home', 'tweets', 'notification']
