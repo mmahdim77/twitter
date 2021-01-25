20280f6bc344d7ea9d090bd8a0dab93a73a946d0
atousa havaseli <atoosa.havaseli@gmail.com>
2021-01-25 23:43:33 +0330

    modal

--- src/pages/login.pages/login.pages.js
+++ src/pages/login.pages/login.pages.js
@@ -1,34 +1,52 @@
-import React, { useState } from "react";
-import { Input, Space, notification } from 'antd';
+import React, { useState,useEffect } from "react";
+import { Input, Space, Modal } from 'antd';
 import Logo from './logo.png'
 import './login.styles.css'
 import 'antd/dist/antd.css'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useHistory } from "react-router-dom";
+import SignUpPage from '../signup.pages/signup.pages'
 
 
 
 
 
-
-export default function LoginPage({ setToken }) {
+export default function LoginPage({ setToken,closed }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isModalVisible, setIsModalVisible] = useState(false);
     let history = useHistory();
 
     function validateForm() {
         return email.length > 0 && password.length > 0;
     }
+    useEffect(() => {
+        if(closed){
+            setIsModalVisible(false);
+        }
+    }, [closed])
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
 
     const handleSubmit = async e => {
         e.preventDefault();
-        let formData = {"email" : email , "password" : password}
+        let formData = { "email": email, "password": password }
         axios.post('http://twitterapifinal.pythonanywhere.com/account/login/', formData).then(
             res => {
                 console.log(res)
-                if (res.status ==200){
-                    history.push("/home/"+email)
+                if (res.status == 200) {
+                    history.push("/home/" + email)
                 }
             }
         )
@@ -59,8 +77,11 @@ export default function LoginPage({ setToken }) {
                     Forgot password?
                 </span>
                 <span>
-                    <Link to="/signup">Sign up for Twitter</Link>
+                    <Link onClick={showModal}>Sign up for Twitter</Link>
                 </span>
+                <Modal className="modal" width="550px" footer={null} closable={false} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                    <SignUpPage fromLogin={true} />
+                </Modal>
             </div>
         </div>
     );
