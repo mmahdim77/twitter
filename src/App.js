2f020b21a21de6667e94b13ff3f15c6b2192c286
mohammad mahdi maghouli <maghoyli.mahdi@gmail.com>
2021-01-25 16:14:32 +0330

    profile, tweet and post added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import PostCard from './components/post-card.components/post-card.components'
+import WriteTweet from './components/write-tweet/write-tweet.components'
+import Profile from './pages/profile/profile.pages'
 import CustomButton from './components/button.components/button.components'
+import moment from 'moment'
+import { Input, Space } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+
+
 function App() {
   const list = ['home', 'tweets', 'notification']
+  const text = "Voluptate dolore fugiat ea ipsum anim eu magna eu labore."
+  const name= "mohammad mahdi"
+  const userName ="@mmahdim"
+  const bio = "Officia non deserunt mollit anim ut esse enim sint est commodo dolor."
+  const date = new Date('January 23, 2020 03:24:00')
   return (
     <div className="App">
-      <PostCard/>
+      <Profile cover="./material/cover2.jpg" name={name} userName={userName} bio={bio} ></Profile>
+      <WriteTweet/>
+      <PostCard name={name} userName={userName} date={Math.floor(((Date.now()-date)/3600000)/24)} postText={text} />
     </div>
   );
 }
---
+++ src/components/write-tweet/write-tweet.components.js
@@ -0,0 +1,59 @@
+
+import React, { useState } from 'react';
+import './write-tweet.styles.css';
+import 'antd/dist/antd.css';
+import { Avatar } from 'antd';
+import PermMediaIcon from '@material-ui/icons/PermMedia';
+import { DownloadOutlined } from '@ant-design/icons';
+
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import { Button } from 'antd';
+import { UserOutlined , CommentOutlined , RetweetOutlined ,LikeOutlined} from '@ant-design/icons';
+// import CustomButton from '../custom-button/custom-button.component' 
+// import {useState , useEffect} from 'react';
+// import axios from 'axios';
+import { Input } from 'antd';
+
+const { TextArea } = Input;
+
+
+
+const WriteTweet = ({avatar , name , userName , date , postText , postMedias }) => {
+    const likedBy =['ali', 'hasan', 'mohsen']
+    const [value, setValue] = useState("");
+    const onChange = ({ target: { value } }) => {
+        setValue(value)
+      };
+    return (
+        <div className="writeTweet">
+            <div className="avatar">
+                    <Avatar size={49} icon={<UserOutlined />} />
+            </div>
+            <div className="mainBody">
+                <div className="textBox">
+                    <TextArea
+                        value={value}
+                        onChange={onChange}
+                        placeholder="What's happening?"
+                        bordered={false}
+                        fullWidth={true}
+                        maxLength="250"
+                        autoSize={{ minRows: 2, maxRows: 6 }}
+                        />
+                </div>
+                <div className="footer">
+                    <div className="attachments">
+                        <PermMediaIcon style={{ fontSize:25 , color:"#1da1f2"}} ></PermMediaIcon>
+                    </div>
+                    <div className="tweetBtn">
+                        <Button type="primary" shape="round" size={"large"}> Tweet </Button>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default WriteTweet
