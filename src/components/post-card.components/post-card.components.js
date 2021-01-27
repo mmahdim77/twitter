6f989a8f04bb4c2c901f8ceff396dc9bb65d7ecf
atousa havaseli <atoosa.havaseli@gmail.com>
2021-01-27 17:57:24 +0330

    like

--- src/components/post-card.components/post-card.components.js
+++ src/components/post-card.components/post-card.components.js
@@ -1,8 +1,7 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './post-card.styles.css';
 import { Avatar } from 'antd';
-import { UserOutlined , CommentOutlined , RetweetOutlined ,LikeOutlined , EllipsisOutlined} from '@ant-design/icons';
+import { UserOutlined, CommentOutlined,HeartFilled ,RetweetOutlined, LikeOutlined, EllipsisOutlined } from '@ant-design/icons';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import IconButton from '@material-ui/core/IconButton';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
@@ -10,45 +9,78 @@ import { Menu, Dropdown } from 'antd';
 
 // import CustomButton from '../custom-button/custom-button.component'
 // import {useState , useEffect} from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 
 
-const PostCard = ({avatar , name , userName , date , postText , postMedias , pk }) => {
-    const likedBy =['ali', 'hasan', 'mohsen']
-    const dateFloor = (Date.now()-new Date(date))/1000
-    const like = () =>{
-        console.log("liked")
-    }
-    const retweet = () =>{
+const PostCard = ({ myUser,token, avatar, name, userName, date, postText, postMedias, pk }) => {
+    const likedBy = ['ali', 'hasan', 'mohsen']
+    const [liked, setLiked] = useState(null);
+    const dateFloor = (Date.now() - new Date(date)) / 1000
+    let formData = { pk: pk }
+    let whoLikes=[]
+    useEffect(() => {
+        console.log(pk)
+        axios.get('http://twitterapifinal.pythonanywhere.com/twitt/get/2' ).then(
+            res => {
+                console.log(res)
+                if (res.data.likes.length > 0) {
+                    res.data.likes.forEach(
+                        function (result) {
+                            whoLikes.push(result.target.username)
+                        }
+                    )
+                }
+            }
+        )
+        .then(
+            () => {
+                const found = whoLikes.find(element => element === myUser.username);
+
+                found ? setLiked(true) : setLiked(false);
+            }
+        )
+    }, [])
+
+
+    const like = () => {
+        setLiked(true)
+        console.log("lie")
+        axios.post('http://twitterapifinal.pythonanywhere.com/twitt/like/', formData, { headers: { 'Authorization': 'Bearer  ' + token } }).then(
+            res => {
+                setLiked(true)
+            }
+        )
+    };
+    const retweet = () => {
         console.log("retweeted")
     }
-    const comments = () =>{
+    const comments = () => {
         console.log("comments")
     }
-    const menu = (id)=>(
+    const menu = (id) => (
         <Menu>
-          {/* <Menu.Item>
+            {/* <Menu.Item>
             <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
               
             </a>
           </Menu.Item> */}
-          <Menu.Item danger>delete {id}</Menu.Item>
+            <Menu.Item danger>delete {id}</Menu.Item>
         </Menu>
-      );
-    const openPostCard =()=>{
-        
+    );
+    const openPostCard = () => {
+
     }
     return (
         <div className="postCard" onClick={openPostCard}>
             {
                 likedBy.length != 0 ?
-                <div className="liked-header">
-                    <LikeOutlined />
-                    <span>liked by {likedBy[0]} and {likedBy.length-1} others</span>
-                </div>
-                :
-                <div></div>
+                    <div className="liked-header">
+                        <LikeOutlined />
+                        <span>liked by {likedBy[0]} and {likedBy.length - 1} others</span>
+                    </div>
+                    :
+                    <div></div>
             }
             <div className="cardBody">
                 <div className="avatar">
@@ -60,29 +92,29 @@ const PostCard = ({avatar , name , userName , date , postText , postMedias , pk 
                             <span className="name">{name}</span>
                             <span className="username">@{userName}</span>
                             {
-                                date!='' ?
+                                date != '' ?
                                     <span className="date">
                                         {
-                                        dateFloor<60 ?
-                                            Math.floor(dateFloor)+' Seconds':
-                                        dateFloor/60<60 ?
-                                            Math.floor(dateFloor/60) + ' Minutes':
-                                        dateFloor/3600<24 ?
-                                            Math.floor(dateFloor/3600) + ' Hours' :
-                                        dateFloor/86400<30 ?
-                                            Math.floor(dateFloor/86400) + ' days':
-                                        dateFloor/86400/30 <12 ?
-                                            Math.floor(dateFloor/86400/30) + ' Months':
-                                            Math.floor(dateFloor/86400/30/12) + ' Years'
+                                            dateFloor < 60 ?
+                                                Math.floor(dateFloor) + ' Seconds' :
+                                                dateFloor / 60 < 60 ?
+                                                    Math.floor(dateFloor / 60) + ' Minutes' :
+                                                    dateFloor / 3600 < 24 ?
+                                                        Math.floor(dateFloor / 3600) + ' Hours' :
+                                                        dateFloor / 86400 < 30 ?
+                                                            Math.floor(dateFloor / 86400) + ' days' :
+                                                            dateFloor / 86400 / 30 < 12 ?
+                                                                Math.floor(dateFloor / 86400 / 30) + ' Months' :
+                                                                Math.floor(dateFloor / 86400 / 30 / 12) + ' Years'
                                         }
                                     </span>
-                                :
+                                    :
                                     <div></div>
 
                             }
                         </div>
                         <div className="setting">
-                            <Dropdown overlay={()=>menu(1)}>
+                            <Dropdown overlay={() => menu(1)}>
                                 <EllipsisOutlined className="ant-dropdown-link" onClick={e => e.preventDefault()} />
                             </Dropdown>
 
@@ -97,9 +129,23 @@ const PostCard = ({avatar , name , userName , date , postText , postMedias , pk 
                 </div>
             </div>
             <div className="actionBar">
-                <ChatBubbleOutlineIcon  style={{ fontSize: 19 }} onClick={comments} ></ChatBubbleOutlineIcon>
-                <RetweetOutlined style={{ fontSize: 19 }} onClick={retweet} />
-                <FavoriteBorderIcon style={{ fontSize: 19 }} onClick={like}></FavoriteBorderIcon>
+                <div className="actionBarBtn">
+                    <ChatBubbleOutlineIcon style={{ fontSize: 19 }} onClick={comments} ></ChatBubbleOutlineIcon>
+                    <span>1</span>
+                </div>
+                <div className="actionBarBtn">
+                    <RetweetOutlined style={{ fontSize: 19 }} onClick={retweet} />
+                    <span>1</span>
+                </div>
+                <div className="actionBarBtn">
+                    {
+                        liked ?
+                            <HeartFilled  style={{ fontSize: 19, color: "#ca2055" }}/>
+                            :
+                            <HeartFilled  style={{ fontSize: 19, color: "gray" }} onClick={like}/>
+                    }
+                    <span>1</span>
+                </div>
             </div>
         </div>
     )
